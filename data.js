b33703978ba9574a7380b1c269e0e6111c2e991d
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-10-29 18:35:47 -0700

    Added data structure for chart

---
+++ data.js
@@ -0,0 +1,15 @@
+// use this data to generate your pie chart
+var data = [
+            ['x', 'Auston Matthews', 'Mitch Marner',],
+            ['salary', 7000, 6800],
+            ['average fantasy points', 6, 4.8],
+            ['dollar per fantasy point', 1166, 1416]
+           ];
+
+/*
+sleep should be color #D81159
+eat should be color #8F2D56
+code should be color #218380
+relax should be color #FBB13C
+other should be color #726DA8
+*/
